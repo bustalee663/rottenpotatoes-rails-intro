9ce4e0d7730dfd16b7ffbbd32ca3b042a8a53156
EC2 Default User <ec2-user@ip-172-31-43-157.us-east-2.compute.internal>
2020-02-06 18:33:31 +0000

    Test sort link

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,7 +12,7 @@ class MoviesController < ApplicationController
 
   def index
     @sort = params[:sort]
-      @movies = Movie.all.order(@sort)
+    @movies = Movie.all.order(@sort)
   end
 
   def new
