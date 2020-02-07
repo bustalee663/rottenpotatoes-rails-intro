b1528adc3ea975a15427a8c47955ca06baa614b8
EC2 Default User <ec2-user@ip-172-31-43-157.us-east-2.compute.internal>
2020-02-07 20:44:25 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,48 +11,17 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @sort_by = params[:sort]
+    @movies = Movie.all.order(@sort_by)
     @all_ratings = Movie.all_ratings
-    @movies = Movie.all
-    redirect = false
-    if params[:sort_by]
-      @sort_by = params[:sort_by]
-      session[:sort_by] = params[:sort_by]
-    elsif session[:sort_by]
-      @sort_by = session[:sort_by]
-      redirect = true
+    if(params[:ratings]) && @sort_by
+      @checkedRatings = params[:ratings].keys
+      @movies = Movie.where(rating: @checkedRatings).order(@sort_by)
+    elsif(params[:ratings])
+      @checkedRatings = params[:ratings].keys
+      @movies = Movie.where(rating: @checkedRatings)
     else
-      @sort_by = nil
-    end
-    
-    if params[:commit] == "Refresh" and params[:ratings].nil?
-      @ratings = nil
-      session[:ratings] = nil
-    elsif params[:ratings]
-      @ratings = params[:ratings]
-      session[:ratings] = params[:ratings]
-    elsif session[:ratings]
-      @ratings = session[:ratings]
-      redirect = true
-    else
-      @ratings = nil
-    end
-    
-    if redirect
-      flash.keep
-      redirect_to movies_path :sort_by=>@sort_by, :ratings=>@ratings
-    end
-
-    if @ratings and @sort_by
-      @movies = Movie.where(:rating => @ratings.keys).find(:all, :order => (@sort_by))
-    elsif @ratings
-      @movies = Movie.where(:rating => @ratings.keys)
-    elsif @sort_by
-      @movies = Movie.find(:all, :order => (@sort_by))
-    else
-      @movies = Movie.all
-    end
-    if !@ratings
-      @ratings = Hash.new
+      @checkedRatings = @all_ratings
     end
   end
 
