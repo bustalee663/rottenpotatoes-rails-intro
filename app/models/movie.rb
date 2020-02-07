26bda5df538cd5462f1b2366c5dc8ec212f16ebe
EC2 Default User <ec2-user@ip-172-31-43-157.us-east-2.compute.internal>
2020-02-07 21:23:24 +0000

    part 3 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,17 @@
 class Movie < ActiveRecord::Base
     def self.ratings
-        Movie.uniq.pluck(:rating)
-    end
+		movie_array = []
+		#https://stackoverflow.com/questions/27791553/rails-correct-way-of-turning-activerecord-relation-to-array
+
+		#Read in the unique ratings
+		all_ratings = Movie.select(:rating).distinct.to_a
+
+		#Get them to strings
+		for item in all_ratings
+			movie_array.push item.rating.to_s
+		end
+
+		#Sort for UI prettyness
+		movie_array.sort!
+	end
 end
