9068e43a563a1a0b44cf601b264c893aa9221d6e
EC2 Default User <ec2-user@ip-172-31-43-157.us-east-2.compute.internal>
2020-02-07 21:25:09 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,52 +11,42 @@ class MoviesController < ApplicationController
   end
 
   def index
-    #Load all the ratings from the movie class
+    #@sort_by = params[:sort]
+    #@movies = Movie.all.order(@sort_by)
     @all_ratings = Movie.ratings
-
-
-    #Need this for the redirect URI to remain restful
-    loaded_session = false
-
-    #Sort problem
+    
+    load = false
+    
     if params[:sort]
-      #If we have a param for sort, we want to set sort to this as well as update the session
       @sort = params[:sort]
       session[:sort] = params[:sort]
     elsif session[:sort]
-      #If there isn't a param but there's a session, we want to load that
       @sort = session[:sort]
-      loaded_session = true
+      load = true
     else
-      #If neither then no sort so set to nil for query
       @sort = nil
     end
-
-      
+    
     if params[:ratings]
-      #use this to keep track in the index view
       @checked_boxes = params[:ratings]
       @movies = Movie.where(rating: @checked_boxes.keys).order(@sort)
       session[:ratings] = params[:ratings]
     elsif session[:ratings]
       @checked_boxes = session[:ratings]
       @movies = Movie.where(rating: @checked_boxes.keys).order(@sort)
-      loaded_session = true
-    else #If not just stick to the sort stuff
-      #sets up the nil case when we have a new program
+      load = true
+    else
       @checked_boxes = []
       @movies = Movie.all.order(@sort)
     end
-
-    #Logic from the readme. If the session was loaded, we want to call redirect to get the URI correct
-    if loaded_session 
-     flash.keep
-     redirect_to movies_path(:sort=>@sort, :ratings=>@checked_boxes)
+    
+    if load
+      flash.keep
+      redirect_to movies_path(:sort => @sort, :ratings => @checked_boxes)
     end
-
+    
     @movies
   end
-
 
   def new
     # default: render 'new' template
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,17 +1,5 @@
 class Movie < ActiveRecord::Base
     def self.ratings
-		movie_array = []
-		#https://stackoverflow.com/questions/27791553/rails-correct-way-of-turning-activerecord-relation-to-array
-
-		#Read in the unique ratings
-		all_ratings = Movie.select(:rating).distinct.to_a
-
-		#Get them to strings
-		for item in all_ratings
-			movie_array.push item.rating.to_s
-		end
-
-		#Sort for UI prettyness
-		movie_array.sort!
-	end
+        Movie.uniq.pluck(:rating)
+    end
 end
